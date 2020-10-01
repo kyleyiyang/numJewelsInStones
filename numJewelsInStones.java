743bf1b3ec79af6a18428d3f2fcf4aadaa7b7d48
kyleyiyang <kyleyiyang@gmail.com>
2020-09-30 22:30:52 -0400

    Create numJewelsInStones.java

---
+++ numJewelsInStones.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        int n=0;
+        int len = S.length();
+        for (int i=0;i<len;i++) {
+            if (J.contains(String.valueOf(S.charAt(i)))) {
+                n++;
+            }
+        }
+        return n;
+    }
+}
+
+//Another solution
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        int n=0;
+        char[] chars = S.toCharArray();
+        int len = S.length();
+        for (int i=0;i<len;i++) {
+            if (J.contains(String.valueOf(chars[i]))) {
+                n++;
+            }
+        }
+        return n;
+    }
+}
